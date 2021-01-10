cb39b5a42a795168a6c63bcad80ae376d1f01e47
Mngz <mngz636@gmail.com>
2021-01-10 10:18:38 +0200

    Update index.php

--- social/index.php
+++ social/index.php
@@ -3,20 +3,6 @@
 
 include 'vendor/autoload.php';
 
-// ...
-class AppKernel extends Kernel
-{
-    public function registerBundles()
-    {
-        $bundles = [
-            // ...
-            new \MartinGeorgiev\SocialPostBundle\SocialPostBundle(),
-        ];
-        return $bundles;
-    }
-    // ...
-}
-
 # Some Symfony container aware class
 //...
 $message = new \MartinGeorgiev\SocialPost\Message($_POST['body']);
